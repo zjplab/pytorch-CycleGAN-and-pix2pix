3a2474343f7ac8857007ffc33a05ed19c6f6a8de
JP Zhang <zjplab@gmail.com>
2019-03-31 15:44:40 -0400

    add one channel transformation

--- data/gray_dataset.py
+++ data/gray_dataset.py
@@ -62,7 +62,7 @@ class GrayDataset(BaseDataset):
         B=AB
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, (w,h))
-        A_transform = get_transform(self.opt, transform_params, grayscale=False)
+        A_transform = get_transform_one_channel(self.opt, transform_params, grayscale=False)
         B_transform = get_transform(self.opt, transform_params, grayscale=False)
 
         A = A_transform(A)
@@ -74,3 +74,34 @@ class GrayDataset(BaseDataset):
         """Return the total number of images in the dataset."""
         # AB_path is a list of file paths 
         return len(self.AB_paths)
+
+def get_transform_one_channel(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
+    transform_list = []
+    if grayscale:
+        transform_list.append(transforms.Grayscale(1))
+    if 'resize' in opt.preprocess:
+        osize = [opt.load_size, opt.load_size]
+        transform_list.append(transforms.Resize(osize, method))
+    elif 'scale_width' in opt.preprocess:
+        transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.load_size, method)))
+
+    if 'crop' in opt.preprocess:
+        if params is None:
+            transform_list.append(transforms.RandomCrop(opt.crop_size))
+        else:
+            transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.crop_size)))
+
+    if opt.preprocess == 'none':
+        transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
+
+    if not opt.no_flip:
+        if params is None:
+            transform_list.append(transforms.RandomHorizontalFlip())
+        elif params['flip']:
+            transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
+
+    if convert:
+        transform_list += [transforms.ToTensor(),
+                           transforms.Normalize((0.5,),
+                                                (0.5, ))]
+    return transforms.Compose(transform_list)
